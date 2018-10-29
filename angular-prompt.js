82e89ae008c7053f290f2ac6649a04a1e08ad6cc
Steven Parradee <sparradee@gmail.com>
2018-10-29 10:51:33 -0400

    Update angular-prompt.js

--- angular-prompt.js
+++ angular-prompt.js
@@ -47,6 +47,18 @@
   
   
   angularPrompt.run(['$templateCache', function($templateCache) {
+    $templateCache.put('promptWindow.template.html', [
+      '<div class="ng-prompt-window">',
+        '<div class="row form">',
+          '<div ng-show="returnOrProcess(options.title) || returnOrProcess(options.drag) || returnOrProcess(options.minimize)" class="prompt-window-title" ng-class="{\'cursor-drag\': returnOrProcess(options.drag)}">',
+            '<div class="pull-right" ng-show="returnOrProcess(options.help)" ng-click="showHelp()" style="margin-left: 5px">',
+              '<span class="glyphicon glyphicon-question-sign cursor-pointer" title="Click for help"></span>',
+            '</div>',
+            '<div class="pull-right" ng-show="returnOrProcess(options.minimize)" ng-click="toggleMinimize()" style="margin-left: 5px">',
+              '<span class="glyphicon glyphicon-modal-window cursor-pointer" ng-class="{\'invert-glyphicon\': returnOrProcess(options.isMinimized)}" title="Click to toggle minimize"></span>',
+            '</div>',
+            '<div class="ng-prompt-title" ng-bind-html="returnOrProcess(options.title) | html"></div>',
+          '</div>',
   
   
   
@@ -241,44 +253,32 @@
   
   
   
+                  // == END ==
+                '</div>',
+                '<div ng-show="returnOrProcess(field.note)" class="well prompt-note">',
+                  '<span ng-bind-html="returnOrProcess(field.note) | html"></span>',
+                '</div>',
+              '</div>',
+            '</div>',
+          '</div>', // End of main form area
+        '</div>',
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+        // Bottom button area
+        '<div ng-hide="returnOrProcess(options.isMinimized)">',
+          '<div class="row" style="margin-top: 10px">',
+            '<div ng-repeat="button in buttons" class="prompt-dialog-buttons" ng-class="{\'col-xs-12\': buttons.length === 1, \'col-xs-6\': buttons.length === 2, \'col-xs-4\': buttons.length === 3, \'col-xs-3\': buttons.length === 5}">',          
+              '<a href class="btn btn-default" tabindex="{{(($index !== (buttons.length - 1)) && !allMandatoryFieldsFilled()) ? -1 : 1000 + $index}}" ng-click="buttonClick($index)" ng-disabled="(($index !== (buttons.length - 1)) && !allMandatoryFieldsFilled()) || disableForm">',
+                '<span ng-bind-html="button | html"></span>',
+              '</a>',
+            '</div>',
+          '</div>',
+        '</div>',
+        // When Minimized
+        '<div ng-show="returnOrProcess(options.isMinimized)" class="minimized-display">',
+          'Window is minimized...',
+        '</div>',
+      '</div>'
+    ].join(' '));
   }]);
 
   // Set the size of a <select> dynamically using ng-size
